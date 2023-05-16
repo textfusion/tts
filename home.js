c4c03d6d70b1704c9199502852b781e74dfffef5
textfusion <133697549+textfusion@users.noreply.github.com>
2023-05-16 21:10:59 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,24 @@
+function UploadBar() {
+    document.getElementById("upload").style.display = "block";
+    document.getElementById("txt").style.display = "none";
+}
+function TextBox(){
+    document.getElementById("upload").style.display = "none";
+    document.getElementById("txt").style.display = "block";
+}
+function Erase(){
+    document.getElementById("Text").value = "";
+}
+function reseti(){
+    document.getElementById("upload-files").value = "";
+    document.getElementById("para").innerHTML="Drag your files here or click in this area.";
+    document.getElementById("im").style.display="";
+    document.getElementById("para").style.lineHeight = "10px";
+}
+function getFileName(myFile){
+    var file = myFile.files[0];  
+    var filename = file.name;
+    document.getElementById("para").style.lineHeight = "210px";
+    document.getElementById("para").innerHTML=filename+" is selected";
+    document.getElementById("im").style.display="none";
+}
